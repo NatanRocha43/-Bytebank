ff8de29055e0e6234ba44ea6074c7215c5c7336b
Natan Rocha <natan.rocha@codeandsoul.com>
2024-03-18 14:56:32 -0300

    Created a logic that meets the requirements

--- js/byteBank.js
+++ js/byteBank.js
@@ -1,37 +1,34 @@
-let saldo = 3000
-const elementoSaldo = document.querySelector('.valor')
-elementoSaldo.textContent = saldo
-
-const elementoFormulario = document.querySelector('.block-nova-transacao form')
-elementoFormulario.addEventListener('submit', function(e){
-    e.preventDefault()
-    if(!elementoFormulario.checkValidity()){
-        alert('Preencha todos os campos da transação')
+var saldo = 3000;
+var elementoSaldo = document.querySelector('.valor');
+elementoSaldo.textContent = saldo.toString();
+var elementoFormulario = document.querySelector('.block-nova-transacao form');
+elementoFormulario.addEventListener('submit', function (e) {
+    e.preventDefault();
+    if (!elementoFormulario.checkValidity()) {
+        alert('Preencha todos os campos da transação');
+        return;
     }
-    const inputTipoTransacao = elementoFormulario.querySelector('#tipoTransacao')
-    const inputValor = elementoFormulario.querySelector('#valor')
-    const inputData = elementoFormulario.querySelector('#data')
-
-    let tipoTransacao = inputTipoTransacao.value
-    let valor = inputValor.value
-    let data = inputData.value
-
-    if(tipoTransacao == 'Depósito'){
-        saldo += valor
-    } else if(tipoTransacao == 'Transferência' || tipoTransacao =='Pagamento de Boleto'){
-        saldo -= valor
-    } else {
-        alert('invalida')
-        return
+    var inputTipoTransacao = elementoFormulario.querySelector('#tipoTransacao');
+    var inputValor = elementoFormulario.querySelector('#valor');
+    var inputData = elementoFormulario.querySelector('#data');
+    var tipoTransacao = inputTipoTransacao.value;
+    var valor = inputValor.valueAsNumber;
+    var data = new Date(inputData.value);
+    if (tipoTransacao == 'Depósito') {
+        saldo += valor;
     }
-    elementoSaldo.textContent = saldo
-
-    const novaTransacao = {
+    else if (tipoTransacao == 'Transferência' || tipoTransacao == 'Pagamento de Boleto') {
+        saldo -= valor;
+    }
+    else {
+        alert('invalida');
+        return;
+    }
+    elementoSaldo.textContent = saldo.toString();
+    var novaTransacao = {
         tipoTransacao: tipoTransacao,
         valor: valor,
         data: data
-    }
-
-    elementoFormulario.reset()
-})
-
+    };
+    elementoFormulario.reset();
+});
